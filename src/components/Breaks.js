096d10bf8ad90982d9efeb90605e4cf8ca5cb248
Henry Crossley <crossh2001@yahoo.com>
2022-06-02 00:11:07 -0700

    made Break Length outline

---
+++ src/components/Breaks.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+export const Breaks = () =>{
+    return(
+        <>
+                <h2>Break Length</h2>
+                <img src=""/>
+            </>
+    );
+};
+
+
+export default Breaks;
