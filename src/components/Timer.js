712df2e66eebf5f6b1c7f3724f014d5c696006a0
Henry Crossley <crossh2001@yahoo.com>
2022-06-11 23:27:30 -0700

    session timer working with bugs

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,11 +3,13 @@ import { useEffect, useState } from 'react';
 
 export const Timer = ({setBreakCount, breakCount, setCount, count}) =>{
 
+    const [seconds, setSeconds] = useState(0);
 
     let min = count;
-    let sec = 0;
+    let sec = seconds;
+    
 
-    useEffect(()=>{
+    /*useEffect(()=>{
     
         let interval = setInterval(()=>{
             clearInterval(interval);
@@ -20,9 +22,45 @@ export const Timer = ({setBreakCount, breakCount, setCount, count}) =>{
             }
         }, 1000);
 
-    }, sec)
+    }, sec)*/
+
  
-    const start_stop = () =>{}
+    /*const start_stop = () =>{
+        let interval = setInterval(()=>{
+            //clearInterval(interval);
+
+            if(sec === 0){
+                if(min!==0){
+                    sec=59;
+                    min=min-1;
+                }
+            }
+        }, 1000);
+    }*/
+
+    const start_stop1 = () => {
+        let interval = setInterval(start_stop, 1000);    
+        console.log("function called");
+        console.log(min+" : "+sec);
+    }
+    let interval = setInterval(start_stop, 1000);
+        
+
+    function start_stop(){
+        clearInterval(interval);
+
+            if(sec === 0){
+                if(min!==0){
+                    //sec=59;
+                    //min=min-1;
+                    setSeconds(59);
+                    setCount(min-1);
+                }
+            }else{
+                //sec=sec-1;
+                setSeconds(sec-1);
+            }
+    }
 
     const reset = () =>{
         setBreakCount(5);
@@ -42,7 +80,7 @@ export const Timer = ({setBreakCount, breakCount, setCount, count}) =>{
 
                 <img id="start_stop"
                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Play_Pause_icon_2283501.svg/1200px-Play_Pause_icon_2283501.svg.png"
-                  width="100px" height="100px" onClick={()=>start_stop()}/>
+                  width="100px" height="100px" onClick={()=>start_stop1()}/>
 
                 <img id="reset"
                  src="https://cdn-icons-png.flaticon.com/512/61/61444.png"
